697fd0c7607112ca59adb330f1868188d0c5f339
cdfelixj <cd.felixj@gmail.com>
2024-02-14 23:14:48 +0800

    second commit

--- loader.js
+++ loader.js
@@ -1,10 +1,13 @@
 window.addEventListener("load", () => {
     const loader = document.querySelector(".loader");
-    const holdLoader = document.querySelector(".holdLoader");
+    const holdLoader = document.querySelector("#preloader");
     holdLoader.classList.add("loader--hidden");
+    
   
     holdLoader.addEventListener("transitionend", () => {
         document.body.removeChild(holdLoader);
+        content.style.display = 'block';
+        
     });
   });
   
